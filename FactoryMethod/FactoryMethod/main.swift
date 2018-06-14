44ecc2edbf9a3d78a45a13d79f971fa491241661
teetbox <teetbox@gmail.com>
2018-06-14 16:05:42 +0800

    Add FactoryMethod Pattern

---
+++ FactoryMethod/FactoryMethod/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  FactoryMethod
+//
+//  Created by Tong Tian on 2018/6/14.
+//  Copyright © 2018 TeetBox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+/*  The Factory Method Pattern defines an interface for creating an object, but lets subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.
+ */
