079861a473e7bb83f19107ac762f95e583ead7cc
teetbox <teetbox@gmail.com>
2018-06-15 21:33:04 +0800

    Add Abstract Factory Pattern

---
+++ AbstractFactory/AbstractFactory/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  AbstractFactory
+//
+//  Created by Tong Tian on 2018/6/15.
+//  Copyright © 2018 TeetBox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+/*  The Abstract Factory Pattern provides an interface for creating families of related or dependent objects without specifying their concrete classes.
+ */
